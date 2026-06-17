4a1f6df4d7267806c0b252c68e5473791b5aa05e
Mybigjay <bibieme8@gmail.com>
2026-06-17 06:34:54 -0400

    Create README.md documentation for IPD Readiness

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ This tool operationalizes your research on **Integrated Project Delivery (IPD)**
 It evaluates how project procurement structure, governance framework, collaboration maturity, and risk allocation align with IPD principles (CCDC 30).
 """)
 
-st.write("**Group 5 Members:** | Blessing Ngozi Emelogu | Collins Gyasi | Danielle Johnson | Omotayo Moyosore Akinwale")
-st.write("**Supervised by: Prof. Babak Rahmani**")
-st.divider()
+#st.write("**Group 5 Members:** | Blessing Ngozi Emelogu | Collins Gyasi | Danielle Johnson | Omotayo Moyosore Akinwale")
+#st.write("**Supervised by: Prof. Babak Rahmani**")
+#st.divider()
 
 # ===================================================
 # 2. ASSESSMENT INPUT LAYER
